a2d08fb967045371d2a7f562af97b7bb2800ad26
Ravitejadarla5 <151873450+Ravitejadarla5@users.noreply.github.com>
2023-12-14 15:31:59 +0530

    Update install-jenkins.sh

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,15 +1,14 @@
 # Jenkins Installation
 sudo apt update -y
-# sudo apt upgrade -y 
-sudo apt install openjdk-17-jre -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+sudo apt install fontconfig openjdk-17-jre -y
+java -version
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
 echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y 
+sudo apt-get update -y
 sudo apt-get install jenkins -y
 sudo systemctl start jenkins
-sudo systemctl status jenkins
 echo "jenkins password"
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
