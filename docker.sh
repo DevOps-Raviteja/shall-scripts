ad8c0b88bf794644ffd592688f08a3356ba0c142
Ravitejadarla5 <151873450+Ravitejadarla5@users.noreply.github.com>
2023-12-20 23:07:00 +0530

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -3,4 +3,4 @@ sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu
 newgrp docker
 sudo systemctl enable docker
-echo "sudo chmod 777 /var/run/docker.sock"
+echo """Execute The Following Command To Run Docker On Ubuntu User -----> sudo chmod 777 /var/run/docker.sock <----- """
