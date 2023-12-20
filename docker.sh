89c5aea328b4d2c4109b2374d52e4fad0f38870f
Ravitejadarla5 <151873450+Ravitejadarla5@users.noreply.github.com>
2023-12-20 23:03:39 +0530

    Update and rename install-docker.sh to docker.sh

--- docker.sh
+++ docker.sh
@@ -2,5 +2,5 @@ sudo apt-get update
 sudo apt-get install docker.io -y
 sudo usermod -aG docker ubuntu
 newgrp docker
-sudo chmod 777 /var/run/docker.sock
 sudo systemctl enable docker
+echo "sudo chmod 777 /var/run/docker.sock"
