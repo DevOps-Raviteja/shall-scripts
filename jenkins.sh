45a94f1206ec5dc7985db8f8f4d85168376c10b6
Ravitejadarla5 <151873450+Ravitejadarla5@users.noreply.github.com>
2024-01-13 20:42:36 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -12,3 +12,11 @@ sudo apt-get install jenkins -y
 sudo systemctl start jenkins
 echo "jenkins password"
 sudo cat /var/lib/jenkins/secrets/initialAdminPassword
+
+
+# INSTALL KUBELETE COMMAND ON JENKINS SERVER
+sudo apt update
+sudo apt install curl -y
+curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
