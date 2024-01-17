07b099b61cc1490b357f20a002a9e9538203e57d
Ravitejadarla5 <151873450+Ravitejadarla5@users.noreply.github.com>
2024-01-17 21:05:19 +0530

    Update ArgoCD.sh

--- ArgoCD.sh
+++ ArgoCD.sh
@@ -8,6 +8,10 @@ kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
 # Fetch Password
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
 
+# To Check Port Number
+kubectl get svc -n argocd
+# Example: https://<Private-ip>:Port
+
 # Clean Up
 kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 kubectl delete namespace argocd
